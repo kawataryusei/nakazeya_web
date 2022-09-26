b947ee72d638177b002c05e8a584b8ed62eb8127
kawataryusei <106373995+kawataryusei@users.noreply.github.com>
2022-09-26 23:57:45 +0900

    initial commit.
    練習です

---
+++ index.js
@@ -0,0 +1,82 @@
+var disp_count = document.getElementById("disp-count");
+var up = document.getElementById("up");
+var count =
+up.addEventListener("click",function(){
+    count += "みかん大福";
+    disp_count.innerHTML = count;
+})
+
+
+var disp_count = document.getElementById("disp-count");
+var up = document.getElementById("up2");
+var count = 
+up.addEventListener("click",function(){
+    count += "いちご大福";
+    disp_count.innerHTML = count;
+})
+
+
+var disp_count = document.getElementById("disp-count");
+var up = document.getElementById("up3");
+var count = 
+up.addEventListener("click",function(){
+    count += "すあま";
+    disp_count.innerHTML = count;
+})
+
+
+var disp_count = document.getElementById("disp-count");
+var up = document.getElementById("up4");
+var count = 
+up.addEventListener("click",function(){
+    count += "鹿の子";
+    disp_count.innerHTML = count;
+})
+
+
+var disp_count = document.getElementById("disp-count");
+var up = document.getElementById("up5");
+var count = 
+up.addEventListener("click",function(){
+    count += "どら焼き";
+    disp_count.innerHTML = count;
+})
+
+
+var disp_count = document.getElementById("disp-count");
+var up = document.getElementById("up6");
+var count = 
+up.addEventListener("click",function(){
+    count += "黄身しぐれ";
+    disp_count.innerHTML = count;
+})
+
+
+var disp_count = document.getElementById("disp-count");
+var up = document.getElementById("up7");
+var count = 
+up.addEventListener("click",function(){
+    count += "柏餅";
+    disp_count.innerHTML = count;
+})
+
+
+var disp_count = document.getElementById("disp-count");
+var up = document.getElementById("up8");
+var count = 
+up.addEventListener("click",function(){
+    count += "深谷の宝石";
+    disp_count.innerHTML = count;
+})
+
+
+var disp_count = document.getElementById("disp-count");
+var up = document.getElementById("up9");
+var count = 
+up.addEventListener("click",function(){
+    count += "千菓子";
+    disp_count.innerHTML = count;
+})
+
+
+
